84a4d4e42ab761939243d5d1452df0065bfad813
LorenzoAm <72865087+LorenzoAm@users.noreply.github.com>
2021-04-27 22:51:43 +0200

    app completed.

--- app/src/main/java/com/example/codicefiscale/MainActivity.kt
+++ app/src/main/java/com/example/codicefiscale/MainActivity.kt
@@ -16,28 +16,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculate.setOnClickListener{
-            if(binding.name.text.toString()=="")
+
+            //controllo campi vuoti
+            if((binding.name.text.toString()=="")||(binding.surname.text.toString()=="")||(binding.date.text.toString()=="")||(binding.gender.checkedRadioButtonId == -1)||(binding.birthplace.text.toString()==""))
             {
                 binding.cf.setText(R.string.error1)
             }
-            else if(binding.surname.text.toString()==""){
+                    //controllo sintassi nome
+            else if((binding.name.text.toString().contains("0"))||(binding.name.text.toString().contains("1"))||(binding.name.text.toString().contains("2"))||(binding.name.text.toString().contains("3"))||(binding.name.text.toString().contains("4"))||(binding.name.text.toString().contains("5"))||(binding.name.text.toString().contains("6"))||(binding.name.text.toString().contains("7"))||(binding.name.text.toString().contains("8"))||(binding.name.text.toString().contains("9")))
+            {
                 binding.cf.setText(R.string.error2)
             }
-            else if(binding.date.text.toString()=="")
+                    //controllo sintassi cognome
+            else if((binding.surname.text.toString().contains("0"))||(binding.surname.text.toString().contains("1"))||(binding.surname.text.toString().contains("2"))||(binding.surname.text.toString().contains("3"))||(binding.surname.text.toString().contains("4"))||(binding.surname.text.toString().contains("5"))||(binding.surname.text.toString().contains("6"))||(binding.surname.text.toString().contains("7"))||(binding.surname.text.toString().contains("8"))||(binding.surname.text.toString().contains("9")))
             {
                 binding.cf.setText(R.string.error3)
             }
-            else if(binding.gender.checkedRadioButtonId == -1)
+                    //controllo date format
+            else if((binding.date.text.toString().length!=10)||(binding.date.text.toString().filter { it == '/' }.count()!=2)) //controllo su lettere
             {
                 binding.cf.setText(R.string.error4)
             }
-            else if(binding.birthplace.text.toString()=="")
+                    //controllo sintassi birthplace
+            else if((binding.birthplace.text.toString().contains("0"))||(binding.birthplace.text.toString().contains("1"))||(binding.birthplace.text.toString().contains("2"))||(binding.birthplace.text.toString().contains("3"))||(binding.birthplace.text.toString().contains("4"))||(binding.birthplace.text.toString().contains("5"))||(binding.birthplace.text.toString().contains("6"))||(binding.birthplace.text.toString().contains("7"))||(binding.birthplace.text.toString().contains("8"))||(binding.birthplace.text.toString().contains("9")))
             {
                 binding.cf.setText(R.string.error5)
             }
             else
             {
-                val output : String
+                var output : String
                 val surname = binding.surname.text.toString().toUpperCase(Locale.ROOT)
                 val name = binding.name.text.toString().toUpperCase(Locale.ROOT)
                 val date = Pattern.compile("/").split(binding.date.text.toString())  //date is an array of string
@@ -50,15 +57,165 @@ class MainActivity : AppCompatActivity() {
                     "F"
                 }
 
-                output = calculateSurname(surname) + calculateName(name) + date[0][3-4]
-
-
+                output = calculateSurname(surname) + calculateName(name) + date[2].substring(2,4) + calculateMonth(date[1]) + calculateDay(gender,date[0]) + calculateMunicipality(birthplace)
+                output += control(output)
 
                 binding.cf.text = output
 
 
             }
         }
+    }
+
+    private fun control(input: String): String
+    {
+        var output = 0
+        var cont = 0
+        while (cont<input.length)
+        {
+            if((cont+1)%2==0)    //carattere in posizione pari
+            {
+                when(input[cont].toString())
+                {
+                    "A","0" -> output += 0
+                    "B","1" -> output += 1
+                    "C","2" -> output += 2
+                    "D","3" -> output += 3
+                    "E","4" -> output += 4
+                    "F","5" -> output += 5
+                    "G","6" -> output += 6
+                    "H","7" -> output += 7
+                    "I","8" -> output += 8
+                    "J","9" -> output += 9
+                    "K" -> output += 10
+                    "L" -> output += 11
+                    "M" -> output += 12
+                    "N" -> output += 13
+                    "O" -> output += 14
+                    "P" -> output += 15
+                    "Q" -> output += 16
+                    "R" -> output += 17
+                    "S" -> output += 18
+                    "T" -> output += 19
+                    "U" -> output += 20
+                    "V" -> output += 21
+                    "W" -> output += 22
+                    "X" -> output += 23
+                    "Y" -> output += 24
+                    "Z" -> output += 25
+                }
+            }
+            else
+            {
+                when(input[cont].toString())
+                {
+                    "A","0" -> output += 1
+                    "B","1" -> output += 0
+                    "C","2" -> output += 5
+                    "D","3" -> output += 7
+                    "E","4" -> output += 9
+                    "F","5" -> output += 13
+                    "G","6" -> output += 15
+                    "H","7" -> output += 17
+                    "I","8" -> output += 19
+                    "J","9" -> output += 21
+                    "K" -> output += 2
+                    "L" -> output += 4
+                    "M" -> output += 18
+                    "N" -> output += 20
+                    "O" -> output += 11
+                    "P" -> output += 3
+                    "Q" -> output += 6
+                    "R" -> output += 8
+                    "S" -> output += 12
+                    "T" -> output += 14
+                    "U" -> output += 16
+                    "V" -> output += 10
+                    "W" -> output += 22
+                    "X" -> output += 25
+                    "Y" -> output += 24
+                    "Z" -> output += 23
+                }
+            }
+            cont++
+        }
+
+        return when(output%26)
+        {
+            0 -> "A"
+            1 -> "B"
+            2 -> "C"
+            3 -> "D"
+            4 -> "E"
+            5 -> "F"
+            6 -> "G"
+            7 -> "H"
+            8 -> "I"
+            9 -> "J"
+            10 -> "K"
+            11 -> "L"
+            12 -> "M"
+            13 -> "N"
+            14 -> "O"
+            15 -> "P"
+            16 -> "Q"
+            17 -> "R"
+            18 -> "S"
+            19 -> "T"
+            20 -> "U"
+            21 -> "V"
+            22 -> "W"
+            23 -> "X"
+            24 -> "Y"
+            25 -> "Z"
+            else -> ""
+
+        }
+
+    }
+
+    private fun calculateMunicipality(municipality: String): String
+    {
+        return when(municipality)
+        {
+            "ROMA" -> "H501"
+            "BOLOGNA" -> "A944"
+            "L'AQUILA" -> "A345"
+            "MILANO" -> "F205"
+            "FIRENZE" -> "D612"
+            else -> ""
+        }
+    }
+
+    private fun calculateDay(gender: String, day: String): String
+    {
+        val output : String
+
+        if(gender == "M")
+        {
+            output = if(day.length==1)
+            {
+                "0$day"
+            }
+            else
+            {
+                day
+            }
+        }
+        else
+        {
+            output = if(day.length==1)
+            {
+                "4$day"
+            }
+            else
+            {
+                val temp = day.toInt() + 40
+                temp.toString()
+            }
+        }
+
+        return output
     }
 
     private fun calculateSurname(surname : String) : String
@@ -115,8 +272,8 @@ class MainActivity : AppCompatActivity() {
             while(out.length < 3)
             {
                 flag = when(name[cont]) {
-                    'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z' -> 1
-                    else -> 0
+                    'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z' -> 1  //e' una consonante
+                    else -> 0                   //e' una vocale
                 }
                 if(flag == 1)
                 {
@@ -135,6 +292,61 @@ class MainActivity : AppCompatActivity() {
         return out
 
     }
+
+    private fun calculateMonth(month : String) : String
+    {
+
+        return when(month)
+        {
+            "01" -> "A"
+            "02" -> "B"
+            "03" -> "C"
+            "04" -> "D"
+            "05" -> "E"
+            "06" -> "H"
+            "07" -> "L"
+            "08" -> "M"
+            "09" -> "P"
+            "10" -> "R"
+            "11" -> "S"
+            "12" -> "T"
+             else -> ""
+
+        }
+    }
+/*
+    private fun checkName(name : String) :String
+    {
+        var out = ""
+        var cont = 0
+        while(cont < name.length)
+        {
+            if(name[cont].toString() != " ")
+                out += name[cont]
+            cont++
+        }
+        return out
+    }
+
+    private fun checkSurname(surname : String) :String
+    {
+        var out = ""
+        var cont = 0
+        while(cont < surname.length)
+        {
+            if((surname[cont].toString() != " ")&&(surname[cont].toString() != "'"))
+                out += surname[cont]
+            cont++
+        }
+        return out
+    }
+
+
+ */
+
+
+
+
 
 
 
